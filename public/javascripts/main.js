373dcbd0093dd3a21300b349c08d715184b78513
Belen <>
2018-08-18 23:49:59 +0200

    clean

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-
-
-$('#far').click(function (places) {
-        let randomDestiny = 0;
-        randomDestiny = Math.random() * (places.length - 0);
-        console.log(randomDestiny);
-        return randomDestiny;
-})
