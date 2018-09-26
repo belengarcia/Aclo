130ebb14a116f71bffd377f278d7e541609a0850
Belen <>
2018-09-26 15:07:28 +0200

    center madrid

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
 const Destiny = require ('../models/destiny.model')
 require('../configs/db.config');
+let places2 = [];
+places2.push(getDestiny('https://i.pinimg.com/originals/e8/e1/e6/e8e1e65b9b6d5f32a739e76ea058c062.jpg', 'Tatoonie', 'Borde exterior de la galaxia. Lo que quiere decir lejísimos', 'Plataforma interestelar, y luego viaje a la velocidad de la luz hasta lo territorios del Borde Exterior'));
+// let places = [{
+//     image: 'https://i.pinimg.com/originals/e8/e1/e6/e8e1e65b9b6d5f32a739e76ea058c062.jpg',
+//     name: 'Tatoonie',
+//     distance: 'Borde exterior de la galaxia. Lo que quiere decir lejísimos',
+//     journey: 'Plataforma interestelar, y luego viaje a la velocidad de la luz hasta lo territorios del Borde Exterior'
+// }, {
+//     image: 'https://www.lagranepoca.com/assets/uploads/2016/01/mordor_by_edli-d2yrha5.jpg',
+//     name: 'Mordor',
+//     distance: 'al otro lado de la Tierra Media',
+//     journey: 'Saliendo desde Hobbiton, más de un mes de camino a pie. El viaje se puede complicar si por el camino te encuentras con orcos, magos oscuros, jinetes negros u otros siervos del Enemigo'
+// }, {
+//     image: 'https://statics.viralizalo.com/virs/2016/01/VIR_46144_4345_las_cronicas_de_narnia_el_leon_la_bruja_y_el_armario.jpg?cb=44959',
+//     name: 'Narnia',
+//     distance: 'Vuelo a la campiña inglesa, una vez allí solo hay que entrar en el armario del profesor Kirke',
+//     journey: 'Vuelo a la campiña inglesa, una vez allí solo hay que entrar en el armario del profesor Kirke'
+// }, {
+//     image: 'https://i.ytimg.com/vi/OYrV4I1b6xQ/maxresdefault.jpg',
+//     name: 'Neo-Tokyo',
+//     distance: '10.755 km (desde Madrid)',
+//     journey: 'Viaja al futuro, tras la tercera guerra mundial. Después coje cualquier vuelo que llegue a Tokio'
+// } ];
 
-let places = [{
-    image: 'https://i.pinimg.com/originals/e8/e1/e6/e8e1e65b9b6d5f32a739e76ea058c062.jpg',
-    name: 'Tatoonie',
-    distance: 'Borde exterior de la galaxia. Lo que quiere decir lejísimos',
-    journey: 'Plataforma interestelar, y luego viaje a la velocidad de la luz hasta lo territorios del Borde Exterior'
-}, {
-    image: 'https://www.lagranepoca.com/assets/uploads/2016/01/mordor_by_edli-d2yrha5.jpg',
-    name: 'Mordor',
-    distance: 'al otro lado de la Tierra Media',
-    journey: 'Saliendo desde Hobbiton, más de un mes de camino a pie. El viaje se puede complicar si por el camino te encuentras con orcos, magos oscuros, jinetes negros u otros siervos del Enemigo'
-}, {
-    image: 'https://statics.viralizalo.com/virs/2016/01/VIR_46144_4345_las_cronicas_de_narnia_el_leon_la_bruja_y_el_armario.jpg?cb=44959',
-    name: 'Narnia',
-    distance: 'Vuelo a la campiña inglesa, una vez allí solo hay que entrar en el armario del profesor Kirke',
-    journey: 'Vuelo a la campiña inglesa, una vez allí solo hay que entrar en el armario del profesor Kirke'
-}, {
-    image: 'https://i.ytimg.com/vi/OYrV4I1b6xQ/maxresdefault.jpg',
-    name: 'Neo-Tokyo',
-    distance: '10.755 km (desde Madrid)',
-    journey: 'Viaja al futuro, tras la tercera guerra mundial. Después coje cualquier vuelo que llegue a Tokio'
-} ];
+function getDestiny(image, name, distance, journey){
+    return {
+        image: image,
+        name: name,
+        distance: distance,
+        journey: journey
+    }
+}
 
 Destiny.create(places)
     .then(
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,40 +1,67 @@
 window.onload = () => {
+    initMap()
+}
 
-    var map, infoWindow;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6
+function initMap() {
+    let map, infoWindow;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.417, lng: -3.703},
+      zoom: 9
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        
+        let posLat = position.coords.latitude;
+        let posLng = position.coords.longitude;
+        
+
+        let minPosLat = posLat + 18;
+        let maxLat = 90;
+
+        let maxLng = 90;
+
+
+       let randomLat = function getRandomLat(min, max) {
+            return Math.random() * ((max - min) + min).toFixed(3) * 1;;
+        }
+        let newPosLat = randomLat(minPosLat, maxLat);
+        
+        let randomLng = function getRandomLng(min, max) {
+            return Math.random() * ((max - min) + min).toFixed(3) * 1;;
+        }
+        let newPosLng = randomLng(posLng, maxLng);
+
+        let newPos = {
+            lat : newPosLat,
+            lng : newPosLng
+        }
+
+        console.log(newPos);
+
+        infoWindow.setPosition(newPos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(newPos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
       });
-      infoWindow = new google.maps.InfoWindow;
-    
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-    
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          infoWindow.open(map);
-          map.setCenter(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
     }
-    
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
+  }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, newPos) {
+      infoWindow.setPosition(newPos);
       infoWindow.setContent(browserHasGeolocation ?
                             'Error: The Geolocation service failed.' :
                             'Error: Your browser doesn\'t support geolocation.');
       infoWindow.open(map);
     }
-
-}
